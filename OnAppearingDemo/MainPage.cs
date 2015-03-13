c30c09be0a5a34b9fd8df1ebb59c3c4d9831b1e8
Chad Michel <chadmichel@me.com>
2015-03-13 13:05:24 -0500

    demo of OnAppearing not working

---
+++ OnAppearingDemo/MainPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace OnAppearingDemo
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var btn = new Button { Text = "Go modal" };
+            btn.Clicked += (object sender, EventArgs e) => 
+                {                   
+                    var modal = new ModalPage();
+                    Navigation.PushModalAsync(modal);
+                };
+            Content = new StackLayout()
+                {
+
+                    Children =
+                        {
+                            new Label { Text = "Main" },
+                            btn
+                        }
+                    };            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            DisplayAlert("OnAppearing!", "OnAppearing!", "Ok");
+        }
+    }
+}
+
---
+++ OnAppearingDemo/ModalPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace OnAppearingDemo
+{
+    public class ModalPage : ContentPage
+    {
+        public ModalPage()
+        {
+            var btn = new Button { Text = "Close" };
+            btn.Clicked += (object sender, EventArgs e) => 
+                {
+                    Navigation.PopModalAsync(); 
+                };
+            Content = new StackLayout()
+            {
+                    
+                Children =
+                {
+                    new Label { Text = "Modal" },
+                    btn
+                }
+            };
+        }
+    }
+}
+
